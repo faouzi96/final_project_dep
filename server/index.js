0efc802a50eec51f959e4642f91f53cb85066322
faouzi96 <faouziboussad@gmail.com>
2022-06-12 16:18:19 +0200

    deploy project

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+const PORT = process.env.PORT || 5001
+
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, "/client/build")))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build", "index.html"))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at PORT ${PORT}`)
+})
